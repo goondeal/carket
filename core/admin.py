20e0f1e61fdf1d10fc6dbd7bfd0ebc35f10d4081
ahmad <ahmed.salamony497@gmail.com>
2023-05-24 09:01:19 +0200

    Register models to admin dashboard

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+admin.site.register(CarMake)
+admin.site.register(CarModel)
+admin.site.register(CarImage)
+admin.site.register(CarBodyType)
+admin.site.register(CarColor)
+admin.site.register(CarDrivetrain)
+admin.site.register(CarFeatureCategory)
+admin.site.register(CarFeature)
+admin.site.register(CarFuelType)
+admin.site.register(CarStatus)
+admin.site.register(Car)
