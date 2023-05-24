3610868a1ec1982ede884d823c0e2262e73f6b7c
ahmad <ahmed.salamony497@gmail.com>
2023-05-24 08:28:25 +0200

    Add CustomUser.city field

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext as _
 from phonenumber_field.modelfields import PhoneNumberField
+
 from .managers import CustomUserManager
+from management.models import City
 
 
 class CustomUser(AbstractUser):
@@ -11,6 +13,8 @@ class CustomUser(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
     phone = PhoneNumberField(null=True, blank=True, unique=True)
     date_of_birth = models.DateField(null=True)
+    city = models.ForeignKey(
+        City, on_delete=models.PROTECT, related_name='users')
 
     _GENDER_CHOICES = (
         ('M', 'Male'),
